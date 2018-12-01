43f532068d7e7cf3480836c1dbf668e806f4da20
Jai <Jai@DESKTOP-7MVJD9P>
2018-12-01 17:06:56 -0500

    Overload

---
+++ IntegrationProject/src/Addition.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Addition {
+    public int add(int a, int b) {
+        
+        int sum = a+b;
+        return sum;
+    }
+    
+    public int add(int a, int b, int c) {
+       
+        int sum = a+b+c; 
+        return sum;
+    }
+
+}
